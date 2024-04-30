e2d6b90f3fbe141ca542100d0aec53c821cd8941
nicolasdca <nicolasdavid193@gmail.com>
2024-04-29 22:09:06 -0300

    Setting items in local storage

--- src/index.js
+++ src/index.js
@@ -4,17 +4,6 @@ import Todo from './js/todo';
 let todos = [];
 let projects = [];
 
-// const myTodo = new Todo(
-//     'Test todo',
-//     'This is a test todo',
-//     '04-23-1997',
-//     'high'
-// );
-// myTodo.displayTodo();
-
-// localStorage.setItem(myTodo.title, JSON.stringify(myTodo));
-// console.log(JSON.parse(localStorage.getItem(myTodo.title)));
-
 function addTodo(title, description, dueDate, priority, projectName) {
     const todo = new Todo(title, description, dueDate, priority, projectName);
     todos.push(todo);
@@ -26,20 +15,14 @@ function addTodo(title, description, dueDate, priority, projectName) {
         projects.push(newProject);
     }
 
-    projects.forEach((project) =>
-        localStorage.setItem(project.name, JSON.stringify(project))
-    );
+    localStorage.setItem('projects', JSON.stringify(projects));
 }
 
+function updateTodo(todoTitle, projectName) {}
+
 addTodo('Test', 'This is a test todo', '04-23-1997', 'high', 'testProject');
-console.table(todos);
-console.table(projects);
-console.log(JSON.parse(localStorage.getItem('testProject')));
 
 addTodo('Test2', 'This is a test todo 2', '04-23-19asas', 'low', 'testProject');
-console.table(todos);
-console.table(projects);
-console.log(JSON.parse(localStorage.getItem('testProject')));
 
 addTodo(
     'Test3',
@@ -48,6 +31,5 @@ addTodo(
     'low',
     'testProject v4'
 );
-console.table(todos);
-console.table(projects);
-console.log(JSON.parse(localStorage.getItem('testProject v4')));
+
+console.table(JSON.parse(localStorage.getItem('projects')));
